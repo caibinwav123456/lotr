e6a64ceea60f505f269e83a54b8fa3b3cd5de38a
CaiBin <342571403@qq.com>
2024-01-23 15:18:02 +0800

    Transfer to VS2019.

--- texture refining on the ring/stdafx.h
+++
@@ -1,4 +0,0 @@
-#include <afxwin.h>
-
-#pragma warning(disable: 4244)
-#pragma warning(disable: 4305)
---
+++ texture refining on the ring/texture refining on the ring/stdafx.h
@@ -0,0 +1,16 @@
+#ifndef VC_EXTRALEAN
+#define VC_EXTRALEAN            // Exclude rarely-used stuff from Windows headers
+#endif
+
+#include <SDKDDKVer.h>
+
+#define _ATL_CSTRING_EXPLICIT_CONSTRUCTORS      // some CString constructors will be explicit
+
+// turns off MFC's hiding of some common and often safely ignored warning messages
+#define _AFX_ALL_WARNINGS
+
+#include <afxwin.h>
+
+#pragma warning(disable: 4244)
+#pragma warning(disable: 4305)
+#pragma warning(disable: 4996)
